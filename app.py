77971455114c1f832fb2397c6f884bae659864c2
HAZIM MUTALIB <hazimutalib96@gmail.com>
2024-01-19 00:01:10 +0800

    update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import streamlit as st
 donations_state_url = "https://raw.githubusercontent.com/MoH-Malaysia/data-darah-public/main/donations_state.csv"
 
 df = pd.read_csv(donations_state_url)
+df.to_csv('test.csv')
 st.write(df)
 
-st.write('hello')
