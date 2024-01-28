78c4de14feb9836468e39cf563d41a06694881e9
HAZIM MUTALIB <hazimutalib96@gmail.com>
2024-01-28 19:21:00 +0800

    update

--- app.py
+++ app.py
@@ -18,28 +18,28 @@ import time
 import schedule
 import telebot
 
-API_TOKEN = '6430193325:AAFkiOTYhb574_owPQVunkNHslzIxRAtNX8'
-bot = telebot.TeleBot(API_TOKEN)
+# API_TOKEN = '6430193325:AAFkiOTYhb574_owPQVunkNHslzIxRAtNX8'
+# bot = telebot.TeleBot(API_TOKEN)
 
-channel_id = '@blood_donatio'
-
-
-def job():
-    print("Job is running...")
-    try:
-        bot.send_message(channel_id, 'Hello, World!')
-        file_path = '.infographics/blood_donation.pdf'
-
-    except:
-        print('Message failed to send')
+# channel_id = '@blood_donatio'
 
 
-schedule.every(1).minutes.do(job)
+# def job():
+#     print("Job is running...")
+#     try:
+#         bot.send_message(channel_id, 'Hello, World!')
+#         file_path = '.infographics/blood_donation.pdf'
+
+#     except:
+#         print('Message failed to send')
 
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# schedule.every(1).minutes.do(job)
+
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
 
 
 
